41618f46d3796d719773abb31d9fe54271f51911
Lifazul Tonmoy <tonmoy.lifazul@gmail.com>
2018-05-31 03:57:00 -0400

    Have start and end buttons working

--- src/index.js
+++ src/index.js
@@ -1,15 +1,52 @@
 import Game from './game';
 
 
+
 document.addEventListener('DOMContentLoaded', () => {
-
-
+  var buttonStart = document.getElementById('buttonStart');
+  var buttonEnd = document.getElementById('buttonEnd');
+  var interval = 20;
   var canvas = document.getElementById("canvas");
   var ctx = canvas.getContext("2d");
-  let interval = 20;
   let game = new Game(ctx, interval);
   game.createRockets(ctx);
   game.createBarrier(ctx);
 
-  setInterval(game.draw, interval);
-})
+  var runSimulation = setInterval(game.draw, interval);
+  let startSimulation = () => {
+    game = new Game(ctx, interval);
+    game.createRockets(ctx);
+    game.createBarrier(ctx);
+    runSimulation = setInterval(game.draw, interval);
+  }
+  let endSimulation = () => {
+    clearInterval(runSimulation);
+  }
+
+  // startSimulation(game, ctx, interval);
+
+  buttonStart.addEventListener('click', startSimulation);
+  buttonEnd.addEventListener('click', endSimulation);
+});
+
+
+
+
+// let endSimulation = () => {
+//   console.log(runSimulation);
+//   clearInterval(runSimulation);
+// }
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//
+//
+//   var canvas = document.getElementById("canvas");
+//   var ctx = canvas.getContext("2d");
+//   let interval = 20;
+//   let game = new Game(ctx, interval);
+//   game.createRockets(ctx);
+//   game.createBarrier(ctx);
+//
+//   setInterval(game.draw, interval);
+// })
