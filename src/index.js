7789111cf09dcfdc5ab5e77a0a83251262bf5f54
Lifazul Tonmoy <tonmoy.lifazul@gmail.com>
2018-07-25 18:00:36 -0400

    On click of canvas shows the canvas x and y position

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
   var buttonStart = document.getElementById('buttonStart');
   var buttonEnd = document.getElementById('buttonEnd');
   var buttonResume = document.getElementById('buttonResume');
+
   var interval = 20;
   var canvas = document.getElementById("canvas");
   var ctx = canvas.getContext("2d");
@@ -11,6 +12,20 @@ document.addEventListener('DOMContentLoaded', () => {
   game.createRockets(ctx);
   game.createBarrier(ctx);
   var gameStart = false;
+
+  function getPosition(event) {
+    var x = event.x;
+    var y = event.y;
+
+    var canvas = document.getElementById("canvas");
+
+    x -= canvas.offsetLeft;
+    x += window.scrollX;
+    y -= canvas.offsetTop;
+    y += window.scrollY;
+
+    console.log("x:" + x + " y:" + y);
+  }
 
   // var runSimulation = setInterval(game.draw, interval);
   let runSimulation;
@@ -40,5 +55,7 @@ document.addEventListener('DOMContentLoaded', () => {
   buttonStart.addEventListener('click', startSimulation);
   buttonEnd.addEventListener('click', endSimulation);
   buttonResume.addEventListener('click', resumeSimulation);
+
+  canvas.addEventListener('click', getPosition)
 
 });
